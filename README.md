ebdf6b2d82c36f2342ccc80c27112f0ae055e522
Matheus <eng.matlima@gmail.com>
2021-06-15 22:22:49 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# BCG-Brasil-Desafio2
-Desafio 2 do processo seletivo para a BGC Brasil
+# Desafio 2 BGC Brasil
 
-Criar um site responsivo para clientes poderem reservar a compra de bonecos minions em miniatura. O intuito é que seja um site simples, basicamente uma landing page com a descrição dos produtos e um formulário para o cliente fazer a reserva do(s) seu(s) minions. Quando a reserva for feita, um email deve ser disparado para o cliente e para \*\*\*\*\*\*@\*\*\*\*\*\*\*\*\*.com.br com os dados preenchidos. Fique a vontade para pensar no texto e no layout.
+Backend do site
---
+++ libs/handler-lib.js
@@ -0,0 +1,36 @@
+// Function that encapsulates default behaviours of lambda functions.
+
+export default function handler(lambda)
+{
+  // async function
+  return async function (event, context)
+  {
+    let body, statusCode;
+    
+    try{
+      // Run the Lambda function and waits for its response. Lambda return goes to the body argument
+      body = await lambda(event, context);
+      // Returns 200, which is the default HTTP response for 'OK'
+      statusCode = 200;
+    }
+    
+    catch (e){
+      // If and error occurs, catchs this error and return it in the 'body' argument;
+      body = { error: e.message };
+
+      // Returns status code 500, which is the default HTTP response for 'Internal Server Error'
+      statusCode = 500;
+    }
+
+      // Return the HTTP response from the server
+    return{
+      statusCode,                                 //Status code
+      body: JSON.stringify(body),                 //Lambda return
+      headers:                                    //CORS headers for Lambda function response (Cross-Origin Resource Sharing)
+      {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    };
+  };
+}
