df798adffcddf508cce0194d98faa59d6aaf5eb5
Matheus <eng.matlima@gmail.com>
2021-06-16 19:46:08 -0300

    Criado um react app

---
+++ bonequinhos/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Bonequinhos!</h1>
+        <p className="text-muted">O seu site de bonecos!</p>
+      </div>
+    </div>
+  );
+}
